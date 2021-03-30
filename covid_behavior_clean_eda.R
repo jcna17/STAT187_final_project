6ef516678a04274dedf70c22707e6d752e07d662
jcna17 <jakecampbell.na@gmail.com>
2021-03-30 14:17:14 -0400

    Continue to refactor cols

--- covid_behavior_clean_eda.R
+++ covid_behavior_clean_eda.R
@@ -6,10 +6,10 @@ data <- raw_data[-1,]
 
 #select columns of interest
 data <- data %>%
-  select("RecordNo" : "#times wash/sani", "gender" : "employment_status")
+  select("endtime" : "#times wash/sani", "gender" : "employment_status")
 
 #rename columns
-new_names <- c("RecordNo", "endtime", 
+new_names <- c("endtime", 
                "state", "qweek", 
                "hh_contact", "non_hh_contact",
                "n_leave_home", "tested", 
@@ -37,16 +37,16 @@ new_names <- c("RecordNo", "endtime",
 
 names(data) <- new_names
 
-#convert columns from character to form that can be used in ML models
+#data summary before recoding
 summary(data)
 
-#convert from character to integer
+#commonly used factor levels
 yn_levels <- c("No", "Yes")
+freq_levels <- c("Not at all", "Rarely", "Sometimes", "Frequently", "Always")
 
 data_recode <- data %>%
   #convert from character to numeric
-  mutate(RecordNo = as.numeric(RecordNo),
-         hh_contact = as.numeric(hh_contact),
+  mutate(hh_contact = as.numeric(hh_contact),
          non_hh_contact = as.numeric(non_hh_contact),
          n_leave_home = as.numeric(n_leave_home),
          age = as.numeric(age),
@@ -61,10 +61,38 @@ data_recode <- data %>%
          lost_taste = factor(lost_taste, levels = yn_levels),
          short_of_breath = factor(short_of_breath, levels = yn_levels),
          no_symp = factor(no_symp, levels = yn_levels)
-         )
+         ) %>%
+  #convert from frequency character to factor
+  mutate(mask = factor(mask, levels = freq_levels),
+         hand_washing = factor(hand_washing, levels = freq_levels),
+         sanitizer = factor(sanitizer, levels = freq_levels),
+         cover_mouth = factor(cover_mouth, levels = freq_levels),
+         avoid_symp_person = factor(avoid_symp_person, levels = freq_levels),
+         avoid_going_out = factor(avoid_going_out, levels = freq_levels),
+         avoid_healthcare = factor(avoid_healthcare, levels = freq_levels),
+         avoid_pub_transport = factor(avoid_pub_transport, levels = freq_levels),
+         avoid_work_not_home = factor(avoid_work_not_home, levels = freq_levels),
+         avoid_school_not_home = factor(avoid_school_not_home, levels = freq_levels),
+         avoid_guests = factor(avoid_guests, levels = freq_levels),
+         avoid_s_gath = factor(avoid_s_gath, levels = freq_levels),
+         avoid_m_gath = factor(avoid_m_gath, levels = freq_levels),
+         avoid_l_gath = factor(avoid_l_gath, levels = freq_levels),
+         avoid_crowds = factor(avoid_crowds, levels = freq_levels),
+         avoid_shopping = factor(avoid_shopping, levels = freq_levels),
+         stop_share_bedroom = factor(stop_share_bedroom, levels = freq_levels),
+         stop_share_meals = factor(stop_share_meals, levels = freq_levels),
+         surface_cleaning = factor(surface_cleaning, levels = freq_levels),
+         avoid_touching_public_objects = factor(avoid_touching_public_objects, levels = freq_levels)
+         ) %>%
+  #convert other characters to factor
+  mutate(tested = factor(tested, levels = c("No, I have not", "Yes, and I tested positive",
+                                            "Yes, and I have not received my results from the test yet",                                                 "Yes, and I tested negative"),
+                                 labels = c("N", "Y+", "Y~", "Y-")))
 
 summary(data_recode)
-unique(data$sanitizer)
+
+unique(data$tested)
+unique(data$hh_tested)
 
 
 
